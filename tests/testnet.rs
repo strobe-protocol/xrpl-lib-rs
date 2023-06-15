5c0a771ba238d473e5c3dcecc0d49e6d24b79dce
Jonathan LEI <me@xjonathan.dev>
2023-06-15 08:06:59 +0000

    test: testnet payment integration test

---
+++ tests/testnet.rs
@@ -0,0 +1,38 @@
+#![cfg(not(target_arch = "wasm32"))]
+
+use url::Url;
+use xrpl_lib::{
+    address::Address, rpc::HttpRpcClient, testnet_faucet::TestnetFaucet,
+    transaction::UnsignedPaymentTransaction,
+};
+
+#[tokio::test]
+async fn testnet_payment() {
+    let faucet = TestnetFaucet::hooks_testnet_v3();
+    let rpc = HttpRpcClient::new(Url::parse("https://hooks-testnet-v3.xrpl-labs.com/").unwrap());
+
+    let new_account = faucet.get_new_account().await.unwrap();
+    assert_eq!(
+        new_account.address,
+        new_account.secret.private_key().public_key().address()
+    );
+
+    let private_key = new_account.secret.private_key();
+
+    let account_info = rpc.account_info(new_account.address).await.unwrap();
+
+    let unsigned_payment = UnsignedPaymentTransaction {
+        account: new_account.address,
+        network_id: 21338,
+        fee: 20,
+        sequence: account_info.account_data.sequence,
+        signing_pub_key: private_key.public_key(),
+        amount: 9000000000,
+        destination: Address::from_base58check("rUUPx6MKAZbaR5zLmUcs9FRou3FhdKa2qD").unwrap(),
+    };
+    let signed_payment = unsigned_payment.sign(&private_key);
+
+    let result = rpc.submit(&signed_payment.to_bytes()).await.unwrap();
+
+    dbg!(result);
+}
